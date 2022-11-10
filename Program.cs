c1fc30fa0cac0df1f7176eeef954301e2dca8b4a
HeliumxD <111045844+HeliumxD@users.noreply.github.com>
2022-11-10 11:05:23 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -12,14 +13,15 @@ namespace Voci
         static void Main(string[] args)
         {
             string antwort = "ja";
+            string antwort2 = "ja";
             do
             {
                 int i = 0;
                 int j = 0;
                 int k = 0;
-                string eingabe;
                 List<string> wort1 = new List<string>();
                 List<string> wort2 = new List<string>();
+                List<string> wort3 = new List<string>();
 
                 Console.WriteLine("Voci lernen einfach gemacht\n\n");
                 do
@@ -33,38 +35,39 @@ namespace Voci
                     antwort = Console.ReadLine();
                 } while (antwort == "ja");
 
-                wort1.Add("Katze");
-                wort2.Add("cat");
-
-
-                foreach (string item in wort1) 
+                do
                 {
-                    Console.WriteLine("Was bedeutet " + item + " übersetzt?");
-                    eingabe = Console.ReadLine();
-
-                    if (item == eingabe) //eig. wort2
+                    foreach (string item in wort1)
                     {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine("\n" + eingabe);
-                        Console.ForegroundColor = ConsoleColor.White;
-                        j++;
-                    }
-                    else if (item != eingabe)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("\n" + eingabe);
-                        //Korrektur
-                        Console.ForegroundColor = ConsoleColor.White;
-                    }
-                }
+                        Console.WriteLine("Was bedeutet " + item + " übersetzt?");
+                        wort3.Add(Console.ReadLine());
 
-                k = (100 / i) * j;
-                Console.WriteLine("Sie haben alle Wörter geübt");
-                Console.WriteLine("Es waren " + j + " von " + i + " Wörtern richtig");
-                Console.WriteLine("Das sind " + k + "%");
-                Console.Write("Möchten sie die Wörter nochmal üben? [ja|nein]");
-                antwort = Console.ReadLine();
-            } while (antwort == "ja");
+                        if (wort3.Equals(wort2))
+                    {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("\n" + wort3);
+                            Console.ForegroundColor = ConsoleColor.White;
+                            j++;
+                        }
+                    else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("\n" + wort3);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                    }
+
+                    k = (100 / i) * j;
+                    Console.WriteLine("Sie haben alle Wörter geübt");
+                    Console.WriteLine("Es waren " + j + " von " + i + " Wörtern richtig");
+                    Console.WriteLine("Das sind " + k + "%");
+                    Console.Write("Möchten sie die Wörter nochmal üben? [ja|nein]");
+                    antwort = Console.ReadLine();
+                } while (antwort == "ja");
+
+                Console.Write("Möchten sie neue Wörter üben? [ja|nein]");
+                antwort2 = Console.ReadLine();
+            } while (antwort2 == "ja");
         }
     }
 }
